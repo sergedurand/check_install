508af6eab76bc821a5a9c4f9947b80a02f41f5e0
Serge Durand <serge.durand@ymail.com>
2023-07-03 14:09:01 +0200

    Update install_tool.sh

---
+++ install_mamba.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget micro.mamba.pm/install.sh
+yes | bash install.sh
+#echo $MAMBA_ROOT_PREFIX
+#echo $MAMBA_EXE
+#source "${HOME}/conda/etc/profile.d/conda.sh"
+#source "${HOME}/conda/etc/profile.d/mamba.sh"
+source ${HOME}/.bashrc
+exec bash
--- install_tool.sh
+++ install_tool.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-wget micro.mamba.pm/install.sh
-yes | bash install.sh
-#echo $MAMBA_ROOT_PREFIX
-#echo $MAMBA_EXE
-#source "${HOME}/conda/etc/profile.d/conda.sh"
-#source "${HOME}/conda/etc/profile.d/mamba.sh"
-source ${HOME}/.bashrc
-exec bash
-micromamba activate
-micromamba create -f pyrat_env.yml -y
-micromamba activate temp_pyrat
-python --version
-python pyrat.pyc -h
-export PYTHONPATH=$PYTHONPATH:$PWD
+bash install_mamba.sh && bash create_env.sh
